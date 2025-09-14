50434c8c470515d647cded92f0cc5ec4bd53915a
roshanpyth <roshanfernandes2011@gmail.com>
2025-09-14 18:54:10 +0100

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -10,12 +10,29 @@ class Rectangle():
       self.rectanglecolor=color
       self.dimension=dimension
    def draw(self):
+      pygame.draw.rect(screen,self.rectanglecolor,self.dimension,5)
+rectangle1=Rectangle("Green",(20,50,40,70))
+
+
+rectangle2=Rectangle("Orange",(50,150,40,70))
+
+
+
+
+
       
-      
+
 while run:
     for i in pygame.event.get():
       if i.type==pygame.QUIT:
          run=False
+      elif i.type==pygame.MOUSEBUTTONDOWN:
+         rectangle1.draw()
+         pygame.display.update()
+         rectangle2.draw()
+         pygame.display.update()
+
+
 
 
    
